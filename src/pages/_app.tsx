a85746270a8cf95b55609a55176d14ae4c74f733
hazem <hazem.muhammad.96@gmail.com>
2023-12-27 12:04:20 +0200

    add head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import type { AppProps } from "next/app";
 import LayoutWrapper from "@components/layout/wrapper/wrapper";
 import "@styles/globals.scss";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <LayoutWrapper>
-            <Component {...pageProps} />
-        </LayoutWrapper>
+        <>
+            <Head>
+                <title>Posts</title>
+            </Head>
+            <LayoutWrapper>
+                <Component {...pageProps} />
+            </LayoutWrapper>
+        </>
     );
 }
