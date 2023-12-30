3331b9104498548165569a95892978d968542b3d
Hazem Muhammad Abdelfattah <hazem.muhammad.96@gmail.com>
2023-12-30 06:11:04 +0200

    update favorites head

--- src/features/favorites/ui/pages/favorites/favorites.tsx
+++ src/features/favorites/ui/pages/favorites/favorites.tsx
@@ -5,12 +5,16 @@ import {
     useFavoritesState,
 } from "../../state/favorites-slice";
 import EmptySavedMessage from "../../components/empty-saved-message";
+import Head from "next/head";
 
 export default function FavoritesPage() {
     const favoritesState = useFavoritesState();
     const dispatch = useAppDispatch();
     return (
         <>
+            <Head>
+                <title>Favorites</title>
+            </Head>
             {favoritesState.posts.length === 0 ? (
                 <EmptySavedMessage />
             ) : (
